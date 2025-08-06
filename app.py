581d5f88de08e2fc510f94788662b6d42cd0a544
deanjroach84 <dean.djroach.roach2@gmail.com>
2025-08-06 03:46:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -267,8 +267,9 @@ def stop_scan(scan_id):
     return jsonify({'error': 'Scan ID not found'}), 404
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
 
