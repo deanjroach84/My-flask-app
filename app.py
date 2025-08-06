bfab7924ecfaf29900a72a3194609d5a6eee40bc
deanjroach84 <dean.djroach.roach2@gmail.com>
2025-08-06 04:19:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, static_folder='static')
 
-app.secret_key = 'your_secret_key_here'  # Change this in production
+app.secret_key = 'd0e24c18990ede4030506468331087c351d8d775d276597e7fc8035360f30059'  # Change this in production
 
 USERS_FILE = 'users.json'
 
@@ -268,3 +268,4 @@ def stop_scan(scan_id):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
